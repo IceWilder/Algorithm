04b779a232db532bd7d7d0ce14f717f15aad02b6
IceWilder <271497566@qq.com>
2022-08-14 22:38:21 +0800

    diameter of binary tree

---
+++ Tree/diameterOfBinaryTree.js
@@ -0,0 +1,3 @@
+function diameterOfBinaryTree(root) {
+    
+}
--- Tree/maxDepthBinaryTree.js
+++ Tree/maxDepthBinaryTree.js
@@ -1,15 +1,26 @@
-var maxDepth = 0;
-function getMaxDepth(root) {
-    dfs(root, 0);
-    return maxDepth;
-}
-function dfs(root, depth) {
-    if (!root) {
-        maxDepth = Math.max(maxDepth, depth);
-        return;
+//求一颗二叉树最大深度
+//先序遍历
+// var maxDepth = 0;
+// function getMaxDepth(root) {
+//     dfs(root, 0);
+//     return maxDepth;
+// }
+// function dfs(root, depth) {
+//     if (!root) {
+//         maxDepth = Math.max(maxDepth, depth);
+//         return;
+//     }
+//     dfs(root.left, depth + 1);
+//     dfs(root.right, depth + 1);
+// }
+//后序遍历
+function dfs(root) {
+    if(!root) {
+        return 0;
     }
-    dfs(root.left, depth + 1);
-    dfs(root.right, depth + 1);
+    var leftMaxDepth = dfs(root.left);
+    var rightMaxDepth = dfs(root.right);
+    return Math.max(leftMaxDepth, rightMaxDepth) + 1;
 }
 var tree = {
     left: {
@@ -25,4 +36,4 @@ var tree = {
         right: {},
     }
 }
-console.log(getMaxDepth(tree));
+console.log(dfs(tree));
